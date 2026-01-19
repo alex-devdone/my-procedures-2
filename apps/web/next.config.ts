58f4306d91450efbbbc6c486f6d6dfbcf0193d08
Alex Shestmintsev <shestmintsev@gmail.com>
2026-01-19 20:48:58 +0100

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,10 @@
+import "@my-procedures-2/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+  transpilePackages: ["shiki"],
+};
+
+export default nextConfig;
